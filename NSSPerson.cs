0ea6332418c8fc83e419c5144c35f7563089fd4d
Daniel Clarke <deliotclarke@gmail.com>
2019-07-18 09:10:36 -0500

    added NSS Person class:

---
+++ NSSPerson.cs
@@ -0,0 +1,12 @@
+namespace StudentExercises
+{
+    public class NSSPerson
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SlackHandle { get; set; }
+        public int Cohort { get; set; }
+
+    }
+}
